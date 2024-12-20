9d559ca146d0f5f61bf2423653aaafc176ad119d
PRINCE0010 <97717613+prince0010@users.noreply.github.com>
2024-12-20 14:22:10 +0800

    fourth commit

--- dist/graphql/resolvers/userResolvers.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.userResolvers = void 0;
-exports.userResolvers = {
-    hello: () => 'Helloooooooooooooo, WORLLLDDD!!',
-};
--- dist/graphql/schema/userSchema.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.userSchema = void 0;
-const graphql_1 = require("graphql");
-exports.userSchema = (0, graphql_1.buildSchema)(`
-  type Query {
-    hello: String
-  }
-`);
